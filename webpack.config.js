a833a62e6d5aa20bdf15fcda8fb5560339a9ec19
sergey <ex-2k@yandex.ru>
2020-10-16 21:41:55 +0300

    dix

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,7 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
+        use: "babel-loader"
       }
     ]
   }
